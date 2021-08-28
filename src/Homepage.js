0126a0067b2d4808ececdb8ec6dc7be2e08e34cc
RA.one <75708799+rahul-singhania223@users.noreply.github.com>
2021-08-28 07:09:48 +0530

    Add files via upload

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,7 +6,7 @@ import Slider from "react-slick";
 import Product from './Product';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import avatar from "./assets/avatar.jpg";
+// import avatar from "./assets/avatar.jpg";
 
 
 
